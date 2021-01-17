6850e8a2a61ba42bf94d17a506e7045df9537cce
kaguachagmira <mantillagka@hotmail.com>
2021-01-17 18:49:10 -0500

    Resubiendo

---
+++ ProjectPrinter/LogicaCuadrado.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using Utilidades;
+
+namespace ProjectPrinter
+{
+    public class LogicaCuadrado
+    {
+        //Variables para el cuadrado
+        const int CENTRADOX = 30;
+        const int CENTRADOY = 20;
+        private float mLado;
+        private float mPerimetro;
+        private float mArea;
+        //Variables para el modo grafico
+        private Graphics mGraficosX;
+        private Graphics mGraficosY;
+        private Graphics mGraficosZ;
+        //Factor de escalamiento
+        private const float SF = 10;
+        //Variables para dibujar
+        private Pen mPen;
+        private int posicion = 0;
+
+        private List<PointF> PuntosT = new List<PointF>();
+        private List<PointF> PuntosB = new List<PointF>();
+
+
+        private PointF mA;
+        private PointF mB;
+        private PointF mC;
+        private PointF mD;
+
+        private PictureBox myPic;
+        public LogicaCuadrado()
+        {
+            mLado = 0.0f;
+            mPerimetro = 0.0f;
+            mArea = 0.0f;
+        }
+        public void InicializaDatos(TextBox lado, TextBox perimetro, TextBox area, PictureBox cuadrado)
+        {
+            lado.Text = "";
+            perimetro.Text = "";
+            area.Text = "";
+            cuadrado.Refresh();
+            mLado = 0.0f;
+            mPerimetro = 0.0f;
+            mArea = 0.0f;
+        }
+        public void LeerDatos(TextBox lado)
+        {
+            try
+            {
+                mLado = float.Parse(lado.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en el Ingreso de Datos" + ex);
+            }
+        }
+        public void AreayPerimetro()
+        {
+            mArea = (float)Math.Pow(mLado, 2);
+            mPerimetro = mLado * 4;
+        }
+        public void Imprimir(TextBox perimetro, TextBox Area)
+        {
+            perimetro.Text = mArea.ToString();
+            Area.Text = mPerimetro.ToString();
+        }
+        public void PuntosPerfil()
+        {
+            mA.X = (0.0f * SF) + CENTRADOX;
+            mA.Y = (0.0f * SF) + CENTRADOY;
+
+            mB.X = (mLado * SF) + CENTRADOX;
+            mB.Y = 0.0f + CENTRADOY;
+
+            mD.X = (mLado * SF) + CENTRADOX;
+            mD.Y = (mLado * SF) + CENTRADOY;
+
+            mC.X = (0.0f * SF) + CENTRADOX;
+            mC.Y = (mLado * SF) + CENTRADOY;
+        }
+        public void CreadoraRelleno(PictureBox[] pictureBoxes, ComboBox color, ListBox[] listas)
+        {
+            Thread.CurrentThread.IsBackground = true;
+            Thread graficoZR = new Thread(new ThreadStart(() => GraficadoraRellenoZ(pictureBoxes, color, listas)));
+            graficoZR.Start();
+            graficoZR.Join();
+        }
+        public void CreadoraContorno(PictureBox[] pictureBoxes, ComboBox color)
+        {
+            PuntosPerfil();
+            Thread graficoZC = new Thread(new ThreadStart(() => GraficadoraContorno(pictureBoxes, color)));
+            graficoZC.Start();
+            graficoZC.Join();
+        }
+        public void GraficadoraContorno(PictureBox[] pictureBoxes, ComboBox color)
+        {
+            //CONTORNO PERSPECTIVA Z
+            mGraficosZ = pictureBoxes[0].CreateGraphics();
+            mGraficosX = pictureBoxes[1].CreateGraphics();
+            mGraficosY = pictureBoxes[2].CreateGraphics();
+
+            mPen = SeleccionarColor(color);
+            mGraficosZ.DrawLine(mPen, mA, mB);
+            mGraficosZ.DrawLine(mPen, mA, mC);
+            mGraficosZ.DrawLine(mPen, mB, mD);
+            mGraficosZ.DrawLine(mPen, mD, mC);
+            //CONTORNO PERSPECTIVA X
+            mGraficosX.DrawLine(mPen, mA, mB);
+            //CONTORNO PERSPECTIVA Y
+            mGraficosY.DrawLine(mPen, mC, mD);
+
+        }
+        public void GraficadoraRellenoZ(PictureBox[] pictureBoxes, ComboBox color, ListBox[] listas)
+        {
+            int verificador = 0;
+            mGraficosZ = pictureBoxes[0].CreateGraphics();
+            mGraficosX = pictureBoxes[1].CreateGraphics();
+            mGraficosY = pictureBoxes[2].CreateGraphics();
+            int rango = (int)mLado * 10;
+            //Z
+            PointF[] PuntosT1;
+            PointF[] PuntosB1;
+
+            PuntosT1 = LinePoints.ObtenerPuntos(mA, mB, rango);
+            PuntosB1 = LinePoints.ObtenerPuntos(mC, mD, rango);
+
+
+            //Y
+            PointF[] PuntosL;
+            PointF[] PuntosR;
+            PuntosL = LinePoints.ObtenerPuntos(mA, mC, rango);
+            PuntosR = LinePoints.ObtenerPuntos(mB, mD, rango);
+            PointF[] PuntosEntreLineas;
+
+
+            do
+            {
+                mPen = SeleccionarColor(color);
+                PuntosEntreLineas = LinePoints.ObtenerPuntos(PuntosL[verificador], PuntosR[verificador], rango);
+                for (int i = 0; i < rango; i++)
+                {
+                    //Z
+                    Thread.Sleep(20);
+                    mGraficosZ.DrawLine(mPen, PuntosT1[i], PuntosB1[i]);
+                    listas[0].Items.Add("X:  " + PuntosT1[i].X.ToString()); listas[1].Items.Add("Y:  " + PuntosT1[i].Y.ToString());
+                    listas[2].Items.Add("X:  " + PuntosB1[i].X.ToString()); listas[3].Items.Add("Y:  " + PuntosB1[i].Y.ToString());
+                    //Y
+                    Point pixel = new Point();
+                    pixel.X = (int)PuntosEntreLineas[i].X;
+                    pixel.Y = (int)PuntosEntreLineas[i].Y;
+                    listas[4].Items.Add("X:  " + PuntosL[i].X.ToString()); listas[5].Items.Add("Y:  " + PuntosL[i].Y.ToString());
+                    listas[6].Items.Add("X:  " + PuntosR[i].X.ToString()); listas[7].Items.Add("Y:  " + PuntosR[i].Y.ToString());
+                    listas[12].Items.Add(pixel.X + "," + pixel.Y );
+                    Rectangle rect = new Rectangle(pixel, new Size(1, 1));
+                    mGraficosY.DrawRectangle(mPen, rect);
+                }
+                //X
+                mGraficosX.DrawLine(mPen, PuntosL[verificador], PuntosR[verificador]);
+                listas[8].Items.Add("X:  " + PuntosL[verificador].X.ToString()); listas[9].Items.Add("Y:  " + PuntosL[verificador].Y.ToString());
+                listas[10].Items.Add("X:  " + PuntosR[verificador].X.ToString()); listas[11].Items.Add("Y:  " + PuntosR[verificador].Y.ToString());
+                verificador++;
+            } while (verificador != rango);
+
+        }
+        public void GraficadoraRellenoX(PictureBox cuadradoX, PictureBox cuadradoY, ComboBox color, int capas)
+        {
+            mGraficosX = cuadradoX.CreateGraphics();
+            int rango = (int)mLado * 10;
+            PointF[] PuntosL;
+            PointF[] PuntosR;
+            PointF Puntos;
+
+            PuntosL = LinePoints.ObtenerPuntos(mA, mC, rango);
+            PuntosR = LinePoints.ObtenerPuntos(mB, mD, rango);
+
+            for (int i = 0; i <= capas; i++)
+            {
+                mGraficosX.DrawLine(mPen, PuntosR[i], PuntosL[i]);
+            }
+
+        }
+        public void GraficadoraRellenoY(PictureBox cuadradoY, ComboBox color)
+        {
+            mGraficosY = cuadradoY.CreateGraphics();
+            int rango = (int)mLado * 10;
+            PointF[] PuntosL;
+            PointF[] PuntosR;
+            PuntosL = LinePoints.ObtenerPuntos(mA, mC, rango);
+            PuntosR = LinePoints.ObtenerPuntos(mB, mD, rango);
+
+            PointF[] PuntosEntreLineas;
+
+            for (int i = 0; i < rango; i++)
+            {
+                PuntosEntreLineas = LinePoints.ObtenerPuntos(PuntosL[i], PuntosR[i], rango);
+                for (int j = 0; j < rango; j++)
+                {
+                    Point pixel = new Point();
+                    pixel.X = (int)PuntosEntreLineas[j].X;
+                    pixel.Y = (int)PuntosEntreLineas[j].Y;
+                    Rectangle rect = new Rectangle(pixel, new Size(2, 2));
+                    Thread.Sleep(20);
+                    mGraficosY.DrawRectangle(mPen, rect);
+                }
+            }
+        }
+        public Pen SeleccionarColor(ComboBox color)
+        {
+            var random = new Random();
+            int aleatorio = random.Next(1, 5);
+
+            if (aleatorio == 1)
+                return new Pen(Color.Blue, 3);
+            if (aleatorio == 2)
+                return new Pen(Color.Red, 3);
+            if (aleatorio == 3)
+                return new Pen(Color.FromArgb(66, 230, 245), 3);
+            if (aleatorio == 4)
+                return new Pen(Color.Green, 3);
+            if (aleatorio == 5)
+                return new Pen(Color.Brown, 3);
+            return new Pen(Color.Black, 3);
+            /*
+            if (color.SelectedItem == "Azul")
+                return new Pen(Color.Blue, 3);
+            if (color.SelectedItem == "Rojo")
+                return new Pen(Color.Red, 3);
+            if (color.SelectedItem == "Amarillo")
+                return new Pen(Color.FromArgb(66, 230, 245), 3);
+            if (color.SelectedItem == "Verde")
+                return new Pen(Color.Green, 3);
+            if (color.SelectedItem == "Café")
+                return new Pen(Color.Brown, 3);
+            return new Pen(Color.Black, 3);*/
+        }
+    }
+}
---
+++ ProjectPrinter/frmCuadrado.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPrinter
+{
+    public partial class frmCuadrado : Form
+    {
+        LogicaCuadrado objCuadrado = new LogicaCuadrado();
+
+        public frmCuadrado()
+        {
+            InitializeComponent();
+            objCuadrado.InicializaDatos(txtLado, txtPerimetro, txtArea, picSquareZ);
+            this.btnContorno.Enabled = !string.IsNullOrWhiteSpace(this.txtLado.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGraficar_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void picSquare_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox6_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListBox[] listaImpresion = {lstZx1, lstZy1, lstZx2, lstZy2,
+                                        lstXx1, lstXy1, lstXx2, lstXy2,
+                                        lstYx1, lstYy1, lstYx2, lstYy2,lstPunto};
+            PictureBox[] pictureBoxes = { picSquareZ, picSquareX, picSquareY };
+            objCuadrado.LeerDatos(txtLado);
+            objCuadrado.AreayPerimetro();
+            objCuadrado.Imprimir(txtPerimetro, txtArea);
+            objCuadrado.CreadoraContorno(pictureBoxes, comboColor);
+            objCuadrado.CreadoraRelleno(pictureBoxes, comboColor, listaImpresion);
+
+        }
+
+        private void frmCuadrado_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLado_TextChanged(object sender, EventArgs e)
+        {
+            this.btnContorno.Enabled = !string.IsNullOrWhiteSpace(this.txtLado.Text);
+        }
+
+        private void groupBox6_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox7_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            PictureBox[] pictureBoxes = { picSquareZ, picSquareX, picSquareY };
+            ListBox[] listaImpresion = {lstZx1, lstZy1, lstZx2, lstZy2,
+                                        lstXx1, lstXy1, lstXx2, lstXy2,
+                                        lstYx1, lstYy1, lstYx2, lstYy2,lstPunto};
+            this.txtLado.Clear();
+
+
+            for (int i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxes[i].Refresh();
+            }
+            for (int j = 0; j < listaImpresion.Length; j++)
+            {
+                listaImpresion[j].Items.Clear();
+            }
+            this.txtArea.Clear();
+            this.txtPerimetro.Clear();
+        }
+    }
+}
